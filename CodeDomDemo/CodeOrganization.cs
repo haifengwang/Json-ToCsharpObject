43e673d2db365ff721517fe523e8a3beae47aea1
白若水 <meng_1999@qq.com>
2016-02-02 20:14:11 +0800

    修改对简单数组的处理，以及空对象的处理
    修改对简单数组的处理，以及空对象的处理

--- CodeDomDemo/CodeOrganization.cs
+++ CodeDomDemo/CodeOrganization.cs
@@ -56,67 +56,129 @@ namespace CodeDomDemo
                        continue;
                    }
 
-                   if (jobj[jb].JsonType == JsonType.String)
-                   {
-                       model.PropertyCollection.Add(new PropertyModel("string", jb.ToString()));
-                       continue;
-                   }
-                   if (jobj[jb].JsonType == JsonType.Boolean)
-                   {
-                       model.PropertyCollection.Add(new PropertyModel("bool", jb.ToString()));
-                       continue;
-                   }
-                   if (jobj[jb].JsonType == JsonType.Number)
-                   {
-                       //需要判断 int型还是 double,这种判断是不准确的
-                       if (Regex.IsMatch(jobj[jb].ToString(), @"^\d+\.\d+$"))
-                       {
-                           model.PropertyCollection.Add(new PropertyModel("double", jb.ToString()));
-                       }
-                       else
-                       {
-                           model.PropertyCollection.Add(new PropertyModel("int", jb.ToString()));
-                       }
-                       continue;
+                   //对象
 
-                   }
-                   //数组
-                   if (jobj[jb].JsonType == JsonType.Array)
-                   {
-                       model.PropertyCollection.Add(new PropertyModel(string.Format("List<{0}>", jb.ToString() + "Model"), jb.ToString()));
-
-                      
-
-                       if (jobj[jb].Count > 0)
-                       {
-                           JsonObject jsobj = jobj[jb][0] as JsonObject;
-                           subGenerateStep(jsobj, jb);
-                       }
-                       else
-                       {
-                           var submodel = new ClassStructureModel();
-                           submodel.NamespeceName = this.NameSpaceName;
-                           submodel.ClassName = jb.ToString() + "Model";
-                           submodel.FileDirPath = this.DirPath;
-                           InnerGenerateCode(null, submodel);
-                       }
-                       continue;
-
-                   }
+                   #region 处理对象
                    //对象
                    if (jobj[jb].JsonType == JsonType.Object)
                    {
 
                        JsonObject jsobj = jobj[jb] as JsonObject;
-                       subGenerateStep(jsobj, jb);
+                       
+                       //如何针对空对象处理呢？ object ?? 新建一个空对象类[目前是新建空对象类]
+                       if (jsobj != null && jsobj.Count > 0)
+                       {
+                           model.PropertyCollection.Add(new PropertyModel(string.Format("{0}", jb.ToString() + "Model"), jb.ToString()));
+
+                           subGenerateStep(jsobj, jb);
+                       }
+                       else
+                       {
+                           model.PropertyCollection.Add(new PropertyModel(string.Format("{0}", "object"), jb.ToString()));
+                       }
+
                        continue;
                    }
+                   #endregion 
+
+                   //数组 
+
+                   #region 处理数组
+                   if (jobj[jb].JsonType == JsonType.Array)
+                   {
+                      
+                       if (jobj[jb].Count > 0)
+                       {
+                           JsonObject jsobj = jobj[jb][0] as JsonObject;
+                           //jsobj==null,就是简单对象，简单对象转换为数组
+                           if (jsobj != null)
+                           {
+                               model.PropertyCollection.Add(new PropertyModel(string.Format("List<{0}>", jb.ToString() + "Model"), jb.ToString()));
+                               subGenerateStep(jsobj, jb);
+                           }
+                           else 
+                           {
+                               //将简单类型的数组转换直接转换为数组
+                               InnerSimpleObjInArray(jobj, model, jb);
+                           }
+                          
+                       }
+                       else
+                       {
+                           //将空数组转换为 object[]数组  
+                           model.PropertyCollection.Add(new PropertyModel("object[]", jb.ToString()));
+                           continue;                         
+                        
+                       }
+                       continue;
+
+                   }
+                   #endregion 
+
+                   //处理简单类型
+                   InnerSimpleObj(jobj, model, jb);
                }
            }
 
            new CreateCs().Produce(model);
        }
 
+       private void InnerSimpleObj(JsonObject jobj, ClassStructureModel model, string jb)
+       {
+           if (jobj[jb].JsonType == JsonType.String)
+           {
+               model.PropertyCollection.Add(new PropertyModel("string", jb.ToString()));
+               // continue;
+           }
+           if (jobj[jb].JsonType == JsonType.Boolean)
+           {
+               model.PropertyCollection.Add(new PropertyModel("bool", jb.ToString()));
+               // continue;
+           }
+           if (jobj[jb].JsonType == JsonType.Number)
+           {
+               //需要判断 int型还是 double,这种判断是不准确的
+               if (Regex.IsMatch(jobj[jb].ToString(), @"^\d+\.\d+$"))
+               {
+                   model.PropertyCollection.Add(new PropertyModel("double", jb.ToString()));
+               }
+               else
+               {
+                   model.PropertyCollection.Add(new PropertyModel("int", jb.ToString()));
+               }
+               // continue;
+
+           }
+       }
+
+       private void InnerSimpleObjInArray(JsonObject jobj, ClassStructureModel model, string jb)
+       {
+           if (jobj[jb][0].JsonType == JsonType.String)
+           {
+               model.PropertyCollection.Add(new PropertyModel("String[]", jb.ToString()));
+               //continue;
+           }
+
+           if (jobj[jb][0].JsonType == JsonType.Boolean)
+           {
+               model.PropertyCollection.Add(new PropertyModel("bool[]", jb.ToString()));
+               //continue;
+           }
+           if (jobj[jb][0].JsonType == JsonType.Number)
+           {
+               //需要判断 int型还是 double,这种判断是不准确的
+               if (Regex.IsMatch(jobj[jb][0].ToString(), @"^\d+\.\d+$"))
+               {
+                   model.PropertyCollection.Add(new PropertyModel("double[]", jb.ToString()));
+               }
+               else
+               {
+                   model.PropertyCollection.Add(new PropertyModel("int[]", jb.ToString()));
+               }
+               // continue;
+
+           }
+       }
 
        private void subGenerateStep(JsonObject jsobj, string jb)
        {
